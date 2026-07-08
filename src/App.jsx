83c3ea669692111c6d757df5e384521168ef83cf
AkshitaGaur-hub <gaurakshita407@gmail.com>
2026-07-08 10:49:28 +0530

    Add background feature

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,27 @@ import SearchBox from "./SearchBox";
 
 const App = () => {
   return (
-    <>
-      <div className='flex flex-col rounded-xl items-center m-20 p-5 mx-auto bg-teal-100 w-[45%]'>
-       <SearchBox />
+    <div className=" pattern-background min-h-screen w-full flex items-center justify-center p-6">
+      <div className='flex flex-col items-center p-5 mx-auto 
+bg-black/30
+backdrop-blur-xl
+border border-white/10
+text-white
+rounded-3xl
+shadow-[0_20px_50px_rgba(0,0,0,0.35)]
+ w-full max-w-[45%] min-w-[320px]'>
+        <SearchBox />
         <button 
           type="button" 
-          className="m-3 text-white rounded-2xl bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-1 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-600 font-medium text-sm px-4 py-2.5 text-center leading-5 cursor-pointer"
+          className="m-3 text-white rounded-2xl bg-gradient-to-r from-pink-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-1 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-600 font-medium text-sm px-4 py-2.5 text-center leading-5 cursor-pointer"
         >
           Search
         </button>
       </div>
-    </>
+
+
+
+    </div>
   )
 }
 
--- src/searchBox.jsx
+++ src/searchBox.jsx
@@ -1,12 +1,39 @@
 import TextField from '@mui/material/TextField';
-import './SearchBox.css';
+import './searchBox.css';
 
 const SearchBox = () => {
   return (
     <div className="searchBox">
       <h1 className='font-bold text-xl m-2'>Search the weather</h1>
       <form>
-        <TextField id="city" label="City" variant="outlined" margin="normal" required />
+        <TextField className='bg-zinc-200' id="city" label="City" variant="outlined" margin="normal" required  sx={{
+    "& .MuiOutlinedInput-root": {
+      backgroundColor: "rgba(0,0,0,0.1)",
+      color: "black",
+
+      "& fieldset": {
+        borderColor: "rgba(0,0,0,0.8)",
+      },
+
+      "&:hover fieldset": {
+        borderColor: "#000",
+      },
+
+      "&.Mui-focused fieldset": {
+        borderColor: "#000",
+        borderWidth: "2px",
+      },
+    },
+
+    "& .MuiInputLabel-root": {
+      color: "rgba(0,0,0,0.8)",
+    },
+
+    "& .MuiInputLabel-root.Mui-focused": {
+      color: "#000",
+    },
+  }}
+   />
       </form>
       
     </div>
